85e122addc4064dd8b285e12d82a16a7db6688d7
Robert <robert.passafaro@gmail.com>
2017-03-01 04:48:16 -0500

    another one down

--- d01/ex05/epur_str.php
+++ d01/ex05/epur_str.php
@@ -4,16 +4,15 @@ function ft_split($arg){
 	return $parts;
 }
 
-$i = 1;
+$i = 0;
 $parts = ft_split($argv[1]);
 while($i < count($parts))
 {
-
-	if (!ctype_space($parts[$i]))
-		print(trim($parts[$i]));
+	$parts[$i] = trim($parts[$i], $character_mask = " \t\n\r\0\x0B");
+	print($parts[$i]);
+	if (!ctype_space($parts[$i]) && $parts[$i] != "")
+		print " ";
 	$i++;
-	if ($i < count($parts))
-		print(" ");
 }
 print "\n";
 ?>

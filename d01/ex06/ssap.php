66b2d232b174ef54939f9e6a996ee081ea4989a7
Robert <robert.passafaro@gmail.com>
2017-03-01 05:40:18 -0500

    ex06

---
+++ d01/ex06/ssap.php
@@ -0,0 +1,27 @@
+<?php
+function ft_split($arg){
+	$parts = preg_split('/\s+/', $arg);
+	return $parts;
+}
+
+    $i = 1;
+    $list = [];
+
+    while ($i < count($argv))
+    {
+        $temp = ft_split($argv[$i]);
+        $a = 0;
+        while ($a < count($temp))
+        {
+            if (!ctype_space($temp[$a]) && $temp[$a] != "")
+                array_push($list, $temp[$a]);
+            $a++;
+        }
+        $i++;
+    }
+    sort($list);
+    foreach ($list as $arr) {
+        print($arr);
+        print("\n");
+    }
+?>

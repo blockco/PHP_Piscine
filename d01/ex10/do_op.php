c98fe980b8e144f34304886ccd18bfc477ce868f
robert passafaro <rpassafa@e1z3r1p3.42.us.org>
2017-03-01 21:40:09 -0800

    done with 10

---
+++ d01/ex10/do_op.php
@@ -0,0 +1,24 @@
+<?php
+function ft_split($arg)
+{
+	$parts = preg_split('/\s+/', $arg);
+	return $parts;
+}
+
+$i = 1;
+$list = [];
+
+while ($i < $argc)
+{
+	$temp = ft_split($argv[$i]);
+	$a = 0;
+	while ($a < count($temp))
+	{
+		if (!ctype_space($temp[$a]) && $temp[$a] != "")
+		array_push($list, $temp[$a]);
+		$a++;
+	}
+	$i++;
+}
+eval ("echo ".implode(" ", $list)."; echo '\n';");
+?>
